5ad65526c441574f77eecc501e7c21aebfad723c
Naman <namansingla830@gmail.com>
2026-05-20 17:46:04 +0530

    use of cin to take input

---
+++ Basics/input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y; // cin is use to take input from the user
+    cout << "Value of x is:" << x << " and value of y is:" << y << endl;
+    return 0;
+}
