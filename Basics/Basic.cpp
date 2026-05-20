5c68fc15b03da20af835bde8c8d6aa6f96905cfc
Naman <namansingla830@gmail.com>
2026-05-20 17:33:13 +0530

    basic how to print name

--- Basics/Basic.cpp
+++ Basics/Basic.cpp
@@ -1,6 +1,13 @@
 // It help to take input or output
 #include <iostream>
 // It help to work in math function 
-#include <math.h>
+// #include <math.h>
 // It help to work with string  
-#include <string>
+// #include <string>
+
+int main(){
+    // cout is use to print the output in the console
+    std::cout << "Hellow World!";
+    std::cout << "Hellow World!";
+    return 0;
+}
