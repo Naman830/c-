8893c2a377a2bb704e5110f1f256d075abf0434d
Naman <namansingla830@gmail.com>
2026-05-20 17:36:55 +0530

    using namespace

--- Basics/Basic.cpp
+++ Basics/Basic.cpp
@@ -1,14 +1,17 @@
 // It help to take input or output
 #include <iostream>
-// It help to work in math function 
+// It help to work in math function
 // #include <math.h>
-// It help to work with string  
+// It help to work with string
 // #include <string>
 
-int main(){
+using namespace std; // It help to avoid writing std:: before every standard library fun or obj
+
+int main()
+{
     // cout is use to print the output in the console
-    std::cout << "Hellow World!" << "\n"; // \n is use to print the output in new line 
-    std::cout << "Hellow World!" << std::endl; // endl is use to print the output in new line and flush the buffer
+    std::cout << "Hellow World!" << "\n"; // \n is use to print the output in new line
+    std::cout << "Hellow World!" << endl; // endl is use to print the output in new line and flush the buffer
     std::cout << "Hellow World!";
     return 0;
 }
