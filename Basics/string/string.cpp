4615c138b0396d39ea290f91d51b8791645a801a
Naman <namansingla830@gmail.com>
2026-05-21 14:58:27 +0530

    strings

---
+++ Basics/string/string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "Hello World";
+    // we check length of string using size()
+    int len = s.size();
+    // last index n-1
+    cout << s[len - 1] << endl;
+    return 0;
+}
