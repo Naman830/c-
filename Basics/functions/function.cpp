8d0eb4b6aad01ecf658c6d267c4938b87afd1216
Naman <namansingla830@gmail.com>
2026-05-21 15:12:49 +0530

    return func

--- Basics/functions/function.cpp
+++ Basics/functions/function.cpp
@@ -13,10 +13,20 @@ void printName(string name){
     cout << "Hello "  << name << endl;
 }
 
+int sum(int num1, int num2){
+    int result = num1 + num2;
+    return result;
+}
 
 int main(){
-    string name;
-    cin >> name;
-    printName(name);
+
+    int num1, num2;
+    cin >> num1 >> num2;
+    int res = sum(num1, num2);
+    cout << res << endl;
+
+    // string name;
+    // cin >> name;
+    // printName(name);
     return 0;
 }
