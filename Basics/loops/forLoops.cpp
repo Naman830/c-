5af8d1b1adb1300d58a36959861309898a78c0d3
Naman <namansingla830@gmail.com>
2026-05-21 15:02:00 +0530

    for loops

---
+++ Basics/loops/forLoops.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // first i is for intialization, second i is for condition and third i is for incremenet or decrement;
+    for (int i = 0; i <= 5; i++){
+        cout << i << " " << endl;
+    }
+    return 0;
+    
+}
