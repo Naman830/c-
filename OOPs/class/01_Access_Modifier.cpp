1bc3a99a2f4cb401b2f8ebc034176eb75b2d7311
Naman <namansingla830@gmail.com>
2026-05-21 15:47:39 +0530

    setter and getter fun

--- OOPs/class/01_Access_Modifier.cpp
+++ OOPs/class/01_Access_Modifier.cpp
@@ -8,31 +8,54 @@ using namespace std;
 // public -----> data and methods accessible to everyone
 // protected -----> data and methods accessible inside the class and it's derived classes
 
+class Teacher
+{
+private:
+    double salary;
 
-class Teacher {
-    public:
+public:
     // properties
     string name;
     string department;
     string subject;
-    double salary;
-    
+
     // methods ( functions) // member functions
-    void changeDepartment(string newDept){
+    void changeDepartment(string newDept)
+    {
         department = newDept;
+    }
+
+    // We can create a function for print the private data member salary
+    // setter to set the private value
+    void setSalary(double s){
+        salary = s;
+    }
+    // getter to get the private value 
+    double getSalary(){
+        return salary;
     }
 };
 
-int main(){
+class Student
+{
+public:
+    string name;
+    int rollNo;
+    string course;
+};
+
+int main()
+{
     // We can create as many objects as we want of the class Teacher
     Teacher t1;
     // We can access the properties and methods of the class using the object t1
     t1.name = "Naman Singla";
     t1.department = "IT";
     t1.subject = "DSA";
-    t1.salary = 50000;
+    t1.setSalary(50000);
     t1.changeDepartment("Computer Science");
 
     cout << "Department: " << t1.department << endl;
+    cout << "Salary: " << t1.getSalary() << endl;
     return 0;
 }
