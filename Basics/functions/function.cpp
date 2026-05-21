b19500e31578840c9547da00c3917718f415be62
Naman <namansingla830@gmail.com>
2026-05-21 15:06:42 +0530

    function observation

---
+++ Basics/functions/function.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Function are a set of code which perform something for you 
+// Function are used to modularise code
+// Function are used to increase useablity of code
+// function are used to use same code multiple times
+
+int main(){
+    return 0;
+}
