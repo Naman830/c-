df01e6d9a2c02447e17f1fef769b52e0d5e726e0
Naman <namansingla830@gmail.com>
2026-05-21 16:14:00 +0530

    types of constructor

--- OOPs/core/03_Constructor.cpp
+++ OOPs/core/03_Constructor.cpp
@@ -6,6 +6,15 @@
 // Only called once(automatically) at time of object creation
 // Memory allocation happens when construtor is called
 
+
+// Types of Constructor:
+// 1. Default or Non-parameterized Constructor: No parameters, which is provided by the compiler if we don't define any constructor. It initalizes the data members with garbage values.
+
+// 2. Parameterized Constructor: It takes parameters to initialize the data members with specific values at the time of object creation.
+
+//3. Copy Constructor: It creates a new object as a copy of an existing object. It takes a reference to an object of the same class as a parameter and initializes the new object with the values of the existing object.
+
+
 #include "bits/stdc++.h"
 using namespace std;
 
@@ -15,11 +24,19 @@ private:
     double salary;
 
 public:
-// Constructor
-// Intialization means assign the value in advance like: department is set to Computer science by default when the object is created
-    Teacher() {
-        department = "Computer Science";
-    };
+    // Constructor
+    
+    // Intialization means assign the value in advance like: department is set to Computer science by default when the object is created
+    // non-parameterized constructor
+    // Teacher() 
+    // {
+    //     department = "Computer Science";
+    // };
+
+    // Parameterized constructor
+    Teacher(string n, string d, string s, double sal){
+
+    }
     string name;
     string department;
     string subject;
@@ -41,7 +58,7 @@ public:
 
 int main()
 {
-    Teacher t1; // Default constructor is called here, which is provided by the compiler if we don't define any constructor. It initalizes the data members with garbage values.
+    Teacher t1; // Default constructor is called here
     t1.name = "Naman Singla";
     t1.subject = "DSA";
     t1.setSalary(50000);
