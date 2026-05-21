b264a4da926dd1dd431e3ce13715d5e993ce2c11
Naman <namansingla830@gmail.com>
2026-05-21 14:35:00 +0530

    data types for numbers

---
+++ Basics/data_types/data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // DATA TYPES FOR NUMBERS
+    // int 
+    int a = 10;
+    // long
+    long x = 1000000000;
+    // float store 4 bytes of data
+    float f = 3.14;
+    //Double store 8 bytes of data
+    double d = 3.199999999999999;
+    // long double store 10 bytes of data 
+    long double ld = 2.9099999999999999999999999;
+    return 0;
+}
