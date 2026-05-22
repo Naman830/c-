7ccd9607861b2516629ea35bd8766591096ef1ae
Naman <namansingla830@gmail.com>
2026-05-22 17:34:59 +0530

    skipping shallow and deep copy constructor

---
+++ OOPs/core/shallow_AND_deep_Constructor.cpp
@@ -0,0 +1,12 @@
+/*
+This is a shallow copy of an object copies all of the memeber values from one object to another 
+
+A copy where:
+
+only values are copied
+pointer addresses are copied directly
+dynamic memory is NOT duplicated
+*/
+
+
+// SKIPPING THIS TOPIC FOR NOW BECAUSE OF EXAM PREPRATION 
