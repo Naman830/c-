cd0cca2f6085e412edfa9ecfdc0e197498407aaa
Naman <namansingla830@gmail.com>
2026-05-22 17:53:17 +0530

    destructor rules added

--- OOPs/core/06_destructor.cpp
+++ OOPs/core/06_destructor.cpp
@@ -1,14 +1,46 @@
 /*
-objects use memory/resources
-after object work is finished
-memory should be cleaned
+    objects use memory/resources
+    after object work is finished
+    memory should be cleaned
 
-That cleaner is called: Destructor
-A destructor is a special function that automatically destroys and cleans an object when its life ends.
+    That cleaner is called: Destructor
+    A destructor is a special function that automatically destroys and cleans an object when its life ends.
 
-It frees:
-memory
-files
-connections
-resources
+    It frees:
+    memory
+    files
+    connections
+    resources
 */
+
+/*
+    Destructor Rules
+    ~ is called tilde.
+    There should be No return type & No parameters
+    Only one destructor allowed
+    You NEVER call destructor manually normally. Compiler automatically calls it.
+*/
+
+#include "bits/stdc++.h"
+using namespace std;
+
+class Student
+{
+public:
+    Student()
+    {
+        cout << "Constructor called\n";
+    }
+
+    ~Student()
+    {
+        cout << "Destructor called\n";
+    }
+};
+int main()
+{
+
+    Student s1;
+
+    return 0;
+}
