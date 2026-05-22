953f281f11349a15864dec9937bd96b54a139857
Naman <namansingla830@gmail.com>
2026-05-22 17:49:05 +0530

    defination of destructor

---
+++ OOPs/core/06_destructor.cpp
@@ -0,0 +1,14 @@
+/*
+objects use memory/resources
+after object work is finished
+memory should be cleaned
+
+That cleaner is called: Destructor
+A destructor is a special function that automatically destroys and cleans an object when its life ends.
+
+It frees:
+memory
+files
+connections
+resources
+*/
--- OOPs/core/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-    // Parametrized way
-    Teacher t1("Naman", "It", "C++", 250000);
-    t1.getInfo();
